6786c75997f6e40bb1ba650e2b9f49b36ba452be
faithnicoletti <faithalicia22@gmail.com>
2023-07-11 16:53:11 -0400

    Initial Commit

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,20 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
+#baby step-usually on a model
+finches = [
+    {'name': 'House Finch', 'habitat': 'Live throughout most of the United States', 'description': 'Small-size bird, large beak, rosey red feathers with streaky brown backs', 'diet': 'Seeds, buds, berries'},
+    {'name': 'American GoldFinch', 'habitat': 'Live mostly of Northern United States', 'description': 'Small bird with bright yellow plumage, a black forehead, and black wings', 'diet': 'Seeds and insects'},
+    {'name': 'Black Rosy Finch', 'habitat': 'Live in the mountains of the western United States', 'description': 'Medium-size bird with black plumages and pink highlights', 'diet': 'Seeds and insects'},
+]
+# Create your views here.
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the finches index.")
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {
+        'finches': finches
+    })
