1876680f32f731dae4c2f441768d3c238baa1a51
faithnicoletti <faithalicia22@gmail.com>
2023-07-11 21:25:12 -0400

    finches added to database through shell

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    habitat = models.TextField(max_length=250)
+    description = models.TextField(max_length=250)
+    diet = models.CharField(max_length=100)
+
+#Changing this instance method
+# does not impact the datbase, therefore
+# no migration needs to be made
+
+def __str__(self):
+    return f'{self.name} ({self.id})'
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
 
-#baby step-usually on a model
-finches = [
-    {'name': 'House Finch', 'habitat': 'Live throughout most of the United States', 'description': 'Small-size bird, large beak, rosey red feathers with streaky brown backs', 'diet': 'Seeds, buds, berries'},
-    {'name': 'American GoldFinch', 'habitat': 'Live mostly of Northern United States', 'description': 'Small bird with bright yellow plumage, a black forehead, and black wings', 'diet': 'Seeds and insects'},
-    {'name': 'Black Rosy Finch', 'habitat': 'Live in the mountains of the western United States', 'description': 'Medium-size bird with black plumages and pink highlights', 'diet': 'Seeds and insects'},
-]
+from .models import Finch
+
 # Create your views here.
 
 def home(request):
@@ -15,6 +11,8 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {
         'finches': finches
-    })
+    }
+)
